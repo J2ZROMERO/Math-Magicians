dedb1c22e0924234c9b7a94f8de729d5c2b202f3
Jhosep <jose-zromero@hotmail.com>
2022-10-31 12:42:30 -0600

    Remove nested project /  Fix linters

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome Jose:
+          {' '}
+          <code> Are you ready?</code>
         </p>
         <a
           className="App-link"
