0292cd3dbe2d8f078ad827f713ff6f932639fac8
Jhosep <jose-zromero@hotmail.com>
2022-11-03 17:24:31 -0600

    code refactoring

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,59 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../style.css';
+
 import calculate from '../logic/calculate';
 
-export default class calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const Calculator = () => {
+  const [data, setdata] = useState({
+    total: null,
+    next: 0,
+    operation: '',
+  });
 
-  componentDidMount() {
-    this.setState({
-      total: null,
-      next: 0,
-      operation: '',
-    });
-  }
+  const handleClick = (e) => {
+    const val = calculate(data, e.target.textContent);
+    setdata(val);
+  };
 
-  handleClick = (e) => {
-    const val = calculate(this.state, e.target.textContent);
-    this.setState(val);
+  const { next, operation, total } = data;
+  let valfinal = '';
+  valfinal = next;
+  if (next === null && operation === null) {
+    valfinal = total;
   }
+  if (next === null && operation === null && total === null) {
+    valfinal = 0;
+  }
+  return (
+    <div id="calculator">
+      <input id="value_result" htmlFor="textarea" placeholder={valfinal} />
+      <button type="button" id="value_AC" onClick={handleClick}>AC</button>
+      <button type="button" id="value_+/-" onClick={handleClick}>+/-</button>
+      <button type="button" id="value_%" onClick={handleClick}>%</button>
+      <button type="button" id="value_/" className="principal_operators" onClick={handleClick}>÷</button>
+      <button type="button" id="value_7" onClick={handleClick}>7</button>
+      <button type="button" id="value_8" onClick={handleClick}>8</button>
+      <button type="button" id="value_9" onClick={handleClick}>9</button>
+      <button type="button" id="value_x" className="principal_operators" onClick={handleClick}>x</button>
+      <button type="button" id="value_4" onClick={handleClick}>4</button>
+      <button type="button" id="value_5" onClick={handleClick}>5</button>
+      <button type="button" id="value_6" onClick={handleClick}>6</button>
+      <button type="button" id="value_-" className="principal_operators" onClick={handleClick}>-</button>
+      <button type="button" id="value_1" onClick={handleClick}>1</button>
+      <button type="button" id="value_2" onClick={handleClick}>2</button>
+      <button type="button" id="value_3" onClick={handleClick}>3</button>
+      <button type="button" id="value_+" className="principal_operators" onClick={handleClick}>+</button>
+      <button type="button" id="value_0" onClick={handleClick}>0</button>
+      <button type="button" id="value_." onClick={handleClick}>.</button>
+      <button type="button" id="value_=" onClick={handleClick} className="principal_operators">=</button>
+    </div>
+  );
+};
 
-  render() {
-    const { next, operation, total } = this.state;
-    let valfinal = '';
-    valfinal = next;
-    if (next === null && operation === null) {
-      valfinal = total;
-    }
-    if (next === null && operation === null && total === null) {
-      valfinal = 0;
-    }
-    return (
-      <div id="calculator">
-        <input id="value_result" htmlFor="textarea" placeholder={valfinal} />
-        <button type="button" id="value_AC" onClick={this.handleClick}>AC</button>
-        <button type="button" id="value_+/-" onClick={this.handleClick}>+/-</button>
-        <button type="button" id="value_%" onClick={this.handleClick}>%</button>
-        <button type="button" id="value_/" className="principal_operators" onClick={this.handleClick}>÷</button>
-        <button type="button" id="value_7" onClick={this.handleClick}>7</button>
-        <button type="button" id="value_8" onClick={this.handleClick}>8</button>
-        <button type="button" id="value_9" onClick={this.handleClick}>9</button>
-        <button type="button" id="value_x" className="principal_operators" onClick={this.handleClick}>x</button>
-        <button type="button" id="value_4" onClick={this.handleClick}>4</button>
-        <button type="button" id="value_5" onClick={this.handleClick}>5</button>
-        <button type="button" id="value_6" onClick={this.handleClick}>6</button>
-        <button type="button" id="value_-" className="principal_operators" onClick={this.handleClick}>-</button>
-        <button type="button" id="value_1" onClick={this.handleClick}>1</button>
-        <button type="button" id="value_2" onClick={this.handleClick}>2</button>
-        <button type="button" id="value_3" onClick={this.handleClick}>3</button>
-        <button type="button" id="value_+" className="principal_operators" onClick={this.handleClick}>+</button>
-        <button type="button" id="value_0" onClick={this.handleClick}>0</button>
-        <button type="button" id="value_." onClick={this.handleClick}>.</button>
-        <button type="button" id="value_=" onClick={this.handleClick} className="principal_operators">=</button>
-      </div>
-    );
-  }
-}
+export default Calculator;
