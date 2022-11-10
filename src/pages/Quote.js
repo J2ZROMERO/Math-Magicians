00a7a9b293bf5501c5ed9a2d91c51ebd296faada
Jhosep <jose-zromero@hotmail.com>
2022-11-10 09:56:19 -0600

    Add routers and styles

---
+++ src/pages/Quote.js
@@ -0,0 +1,17 @@
+/* eslint-disable max-len */
+import React from 'react';
+import Header from '../components/Header';
+
+const Quote = () => (
+  <>
+    <Header />
+    <div>
+      <p>
+        Mathematics is not about number, equations, computacions
+        or algorithms: it is about understanding. -William Paul Thruston
+      </p>
+    </div>
+  </>
+);
+
+export default Quote;
